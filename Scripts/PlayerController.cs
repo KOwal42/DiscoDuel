1516eaea4e8234c1294d2ee943fefce10a1f320a
flasher49 <flasher49@gmail.com>
2015-11-19 22:37:40 +0100

    Sprawdzanie kombinacji pierwsza wersja

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+
+	private string combination;
+	private string [] combos;
+
+	void addToCombination(string key){
+		combination += key;
+	}
+
+	void resetCombination(){
+		combination = "";
+	}
+
+	string getCombination(){
+		return combination;
+	}
+
+
+	void Start () {
+		combos = new string[] { 
+			"RU", "RR", "RD",
+			"LU", "LR", "LD",
+			"UU", "DD",
+			"UDRU", "UDRR", "UDRD"
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			addToCombination("U");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			addToCombination("D");
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			addToCombination("L");
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			addToCombination("R");
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (Array.IndexOf(combos, getCombination()) != -1){
+				Debug.Log(getCombination() + " : OK");
+			} else {
+				Debug.Log(getCombination() + " : DUPA");
+			} 
+			resetCombination();
+		}
+
+	}
+}
